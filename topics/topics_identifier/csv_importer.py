26aa6efec2f5081068708c252335bc1b6453e0ec
adavideo <adavideo@gmail.com>
2020-05-21 08:10:01 +0200

    Added csv file type news_with_author

--- topics/topics_identifier/csv_importer.py
+++ topics/topics_identifier/csv_importer.py
@@ -2,9 +2,15 @@ import csv, io
 
 # PROCESS DATA
 
-def process_news(column):
+def process_news_without_author(column):
     title = column[4]
     content = column[5]
+    text = "title: " + title + "\ncontent: " + content
+    return text
+
+def process_news_with_author(column):
+    title = column[5]
+    content = column[6]
     text = "title: " + title + "\ncontent: " + content
     return text
 
@@ -13,8 +19,10 @@ def process_comment(column):
     return content
 
 def process_csv_line(column, file_type):
-    if file_type == "news":
-        result = process_news(column)
+    if file_type == "news_without_author":
+        result = process_news_without_author(column)
+    elif file_type == "news_with_author":
+        result = process_news_with_author(column)
     elif file_type == "comments":
         result = process_comment(column)
     else:
@@ -32,8 +40,9 @@ def process_data(io_string, file_type):
 # IDENTIFY FILE TYPE
 
 def get_headers_types():
-    headers_types = [ {"file_type":"news", "header": '"link_id","link_date","link_uri","link_url_title","link_title","link_content"'},
-                    {"file_type":"comments", "header": '"comment_id","comment_link_id","comment_user_id","comment_date","comment_content"'}
+    headers_types = [ {"file_type":"news_without_author", "header": '"link_id","link_date","link_uri","link_url_title","link_title","link_content"'},
+                      {"file_type":"news_with_author", "header": '"link_id","link_author","link_date","link_uri","link_url_title","link_title","link_content"'},
+                      {"file_type":"comments", "header": '"comment_id","comment_link_id","comment_user_id","comment_date","comment_content"'},
                     ]
     return headers_types
 
