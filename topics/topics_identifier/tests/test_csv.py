ff98cca4a5e0c9faaa4b9a9ae78b35687a7883bb
adavideo <adavideo@gmail.com>
2020-05-21 09:40:30 +0200

    Adding tests for process_csv_line

--- topics/topics_identifier/tests/test_csv.py
+++ topics/topics_identifier/tests/test_csv.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .examples_csv import *
-from topics_identifier.csv_importer import process_news_without_author, process_news_with_author, process_comment
+from topics_identifier.csv_importer import process_news_without_author, process_news_with_author, process_comment, process_csv_line
 
 
 class CSVProcessDataTests(TestCase):
@@ -20,4 +20,24 @@ class CSVProcessDataTests(TestCase):
     def test_comment(self):
         column = comment_example1
         result = process_comment(column)
+        self.assertEqual(result["content"], comment_content1)
+
+    def test_process_csv_line_news_without_author(self):
+        file_type = "news_without_author"
+        column = news_without_author_example1
+        result = process_csv_line(column, file_type)
+        self.assertEqual(result["title"], news_without_author_title1)
+        self.assertEqual(result["content"], news_without_author_content1)
+
+    def test_process_csv_line_news_with_author(self):
+        file_type = "news_with_author"
+        column = news_with_author_example1
+        result = process_csv_line(column, file_type)
+        self.assertEqual(result["title"], news_with_author_title1)
+        self.assertEqual(result["content"], news_with_author_content1)
+
+    def test_process_csv_line_comment(self):
+        file_type = "comments"
+        column = comment_example1
+        result = process_csv_line(column, file_type)
         self.assertEqual(result["content"], comment_content1)
