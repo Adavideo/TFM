885445bb964c3858805bcf0a83ef2546e6caba19
adavideo <adavideo@gmail.com>
2020-05-21 10:01:05 +0200

    Erasing the news without author file type for csv import

--- topics/topics_identifier/csv_importer.py
+++ topics/topics_identifier/csv_importer.py
@@ -2,13 +2,7 @@ import csv, io
 
 # PROCESS DATA
 
-def process_news_without_author(column):
-    title = column[4]
-    content = column[5]
-    result = { "title": title, "content": content}
-    return result
-
-def process_news_with_author(column):
+def process_news(column):
     title = column[5]
     content = column[6]
     result = { "title": title, "content": content}
@@ -20,10 +14,8 @@ def process_comment(column):
     return result
 
 def process_csv_line(column, file_type):
-    if file_type == "news_without_author":
-        result = process_news_without_author(column)
-    elif file_type == "news_with_author":
-        result = process_news_with_author(column)
+    if file_type == "news":
+        result = process_news(column)
     elif file_type == "comments":
         result = process_comment(column)
     else:
@@ -41,8 +33,7 @@ def process_data(io_string, file_type):
 # IDENTIFY FILE TYPE
 
 def get_headers_types():
-    headers_types = [ {"file_type":"news_without_author", "header": '"link_id","link_date","link_uri","link_url_title","link_title","link_content"'},
-                      {"file_type":"news_with_author", "header": '"link_id","link_author","link_date","link_uri","link_url_title","link_title","link_content"'},
+    headers_types = [ {"file_type":"news", "header": '"link_id","link_author","link_date","link_uri","link_url_title","link_title","link_content"'},
                       {"file_type":"comments", "header": '"comment_id","comment_link_id","comment_user_id","comment_date","comment_content"'},
                     ]
     return headers_types
--- topics/topics_identifier/tests/test_csv.py
+++ topics/topics_identifier/tests/test_csv.py
@@ -1,40 +1,27 @@
 from django.test import TestCase
 from .examples_csv import *
-from topics_identifier.csv_importer import process_news_without_author, process_news_with_author, process_comment, process_csv_line, get_file_type
+from topics_identifier.csv_importer import process_news, process_comment, process_csv_line, get_file_type
 
 
 class CSVProcessDataTests(TestCase):
 
-    def test_news_without_author(self):
-        column = news_without_author_example1
-        result = process_news_without_author(column)
-        self.assertEqual(result["title"], news_without_author_title1)
-        self.assertEqual(result["content"], news_without_author_content1)
-
-    def test_news_with_author(self):
-        column = news_with_author_example1
-        result = process_news_with_author(column)
-        self.assertEqual(result["title"], news_with_author_title1)
-        self.assertEqual(result["content"], news_with_author_content1)
+    def test_news(self):
+        column = news_example1
+        result = process_news(column)
+        self.assertEqual(result["title"], news_title1)
+        self.assertEqual(result["content"], news_content1)
 
     def test_comment(self):
         column = comment_example1
         result = process_comment(column)
         self.assertEqual(result["content"], comment_content1)
 
-    def test_process_csv_line_news_without_author(self):
-        file_type = "news_without_author"
-        column = news_without_author_example1
+    def test_process_csv_line_news(self):
+        file_type = "news"
+        column = news_example1
         result = process_csv_line(column, file_type)
-        self.assertEqual(result["title"], news_without_author_title1)
-        self.assertEqual(result["content"], news_without_author_content1)
-
-    def test_process_csv_line_news_with_author(self):
-        file_type = "news_with_author"
-        column = news_with_author_example1
-        result = process_csv_line(column, file_type)
-        self.assertEqual(result["title"], news_with_author_title1)
-        self.assertEqual(result["content"], news_with_author_content1)
+        self.assertEqual(result["title"], news_title1)
+        self.assertEqual(result["content"], news_content1)
 
     def test_process_csv_line_comment(self):
         file_type = "comments"
