a96d65359cf28f7e40d6da535611d13ef0e6aaaa
adavideo <adavideo@gmail.com>
2020-05-22 09:03:52 +0200

    Moving csv import to a new page

--- topics/topics_identifier/urls.py
+++ topics/topics_identifier/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('import_files', views.import_files, name='import_files'),
 ]
--- topics/topics_identifier/views.py
+++ topics/topics_identifier/views.py
@@ -3,13 +3,17 @@ from .forms import *
 from .csv_importer import process_csv
 
 def index(request):
+    template = "topics_identifier/home.html"
+    context = {}
+    return render(request, template, context)
+
+def import_files(request):
     template = "topics_identifier/file_upload.html"
     form = ImportCSVForm()
     context = {'form': form }
 
     if request.method == "POST":
         file = request.FILES['file']
-        result = process_csv(file)
-        context["result"] = result
+        context["result"] = process_csv(file)
 
     return render(request, template, context)
