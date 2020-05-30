8d694800a90bbff51456e092dde011991e9e5252
Adavideo <adavideo@gmail.com>
2020-05-30 17:33:03 +0200

    Shows on the terminal the progress of the csv processing

--- topics/topics_identifier/csv_importer.py
+++ topics/topics_identifier/csv_importer.py
@@ -31,12 +31,16 @@ def process_csv_line(column, file_type, data_name, file_number):
         result = "File type "+ str(file_type) + " not recognised"
     return result
 
-def process_data(io_string, file_type, data_name):
+def process_data(csv_reader, file_type, data_name, total_num_registers):
     result = []
-    file_number = count_existing_files(type=file_type) + 1
-    for column in csv.reader(io_string, delimiter=',', quotechar='"'):
+    existing_files = count_existing_files(type=file_type)
+    file_number = existing_files + 1
+    for column in csv_reader:
         r = process_csv_line(column, file_type, data_name, file_number)
         result.append(r)
+        register = file_number - existing_files
+        completed = 100.0 * register / total_num_registers
+        print(str(register)+" of "+str(total_num_registers)+" registers. "+str(completed)+"% completed")
         file_number += 1
     return result
 
@@ -60,14 +64,26 @@ def get_file_type(header):
 
 # READ AND PROCESS CSV FILE
 
-def process_csv(file):
-    data_name = file.name.split('.')[0]
-    file_content = file.read().decode('UTF-8')
+def get_csv_reader_and_header(file_content):
     io_string = io.StringIO(file_content)
     header = next(io_string)
+    csv_reader = csv.reader(io_string, delimiter=',', quotechar='"')
+    return csv_reader, header
+
+def get_number_of_registers(file_content):
+    csv_reader, _ = get_csv_reader_and_header(file_content)
+    registers = sum(1 for row in csv_reader)
+    return registers
+
+def process_csv(file):
+    file_content = file.read().decode('UTF-8')
+    csv_reader, header = get_csv_reader_and_header(file_content)
     file_type = get_file_type(header)
     if file_type == "incorrect":
         result = ["Incorrect file type"]
     else:
-        result = process_data(io_string, file_type, data_name)
+        data_name = file.name.split('.')[0]
+        num_registers = get_number_of_registers(file_content)
+        print("\nProcessing "+file_type+" csv file with "+str(num_registers)+" registers.")
+        result = process_data(csv_reader, file_type, data_name, num_registers)
     return result
