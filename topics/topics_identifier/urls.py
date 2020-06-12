1898ece9a5d27c9ecd91e4774d865bb826c3947a
Adavideo <adavideo@gmail.com>
2020-06-12 14:56:21 +0200

    Clusters page to see all clusters

--- topics/topics_identifier/urls.py
+++ topics/topics_identifier/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('import_files', views.import_files_view, name='import_files'),
     path('generate_dataset', views.generate_dataset_view, name='generate_dataset'),
     path('cluster_data', views.cluster_data_view, name='cluster_data'),
+    path('clusters', views.clusters_view, name='clusters'),
 ]
--- topics/topics_identifier/views.py
+++ topics/topics_identifier/views.py
@@ -4,6 +4,7 @@ from .csv_importer import process_csv
 from .data_importer import load_and_store_dataset
 from .input_output_files import load_dataset, short_texts_filenames
 from .data_classifier import cluster_data
+from .models import Cluster
 
 def index_view(request):
     template = "topics_identifier/home.html"
@@ -46,7 +47,19 @@ def cluster_data_view(request):
         dataset_name = request.POST["dataset_name"]
         context["dataset_name"] = dataset_name
         dataset = load_dataset(dataset_name)
-        if dataset:
-            context["clusters"] = cluster_data(dataset)
-            context["dataset_description"] = dataset.DESCR
+        context["dataset_description"] = dataset.DESCR
+        cluster_data(dataset, dataset_name)
+        context["clusters"] = Cluster.objects.filter(dataset=dataset_name)
+    return render(request, template, context )
+
+def clusters_view(request):
+    clusters = Cluster.objects.all()
+    clusters_list = []
+    for cluster in clusters:
+        docs = cluster.documents()
+        cluster_info = {"cluster": cluster, "documents": docs}
+        clusters_list.append(cluster_info)
+
+    template = "topics_identifier/clusters.html"
+    context = {"clusters_list": clusters_list }
     return render(request, template, context )
