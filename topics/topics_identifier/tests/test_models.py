07879ad13165f9a8301357f177fb9b4630f6e20e
Adavideo <adavideo@gmail.com>
2020-06-15 08:43:07 +0200

    Created functions to mock clusters and documents

--- topics/topics_identifier/tests/test_models.py
+++ topics/topics_identifier/tests/test_models.py
@@ -1,34 +1,29 @@
 from django.test import TestCase
 from topics_identifier.models import Cluster, Document, find_or_create_document
-from .examples_text_datasets_and_documents import example_documents_short, example_documents_long, example_terms1
+from .examples_text_datasets_and_documents import test_dataset, example_documents, create_example_document, create_example_cluster
 
 class DocumentTests(TestCase):
 
     def test_create_document_short(self):
-        content = example_documents_short[0]
-        doc = Document(content=content)
-        doc.save()
-        self.assertEqual(doc.content, content)
+        doc = create_example_document(type="short")
+        self.assertEqual(doc.content, example_documents["short"][0])
         self.assertEqual(str(doc), "Document 1")
 
     def test_create_document_long(self):
-        content = example_documents_long[0]
-        doc = Document(content=content)
-        doc.save()
-        self.assertEqual(doc.content, content)
+        doc = create_example_document(type="long")
+        self.assertEqual(doc.content, example_documents["long"][0])
         self.assertEqual(str(doc), "Document 1")
 
     def test_find_or_create_document_created(self):
-        content = example_documents_short[1]
-        created_doc = Document(content=content)
-        created_doc.save()
+        created_doc = create_example_document(type="short")
+        content = example_documents["short"][0]
         found_doc = find_or_create_document(content)
         self.assertEqual(found_doc.content, content)
         self.assertEqual(found_doc, created_doc)
         self.assertEqual(str(found_doc), str(created_doc))
 
     def test_find_or_create_document_not_created(self):
-        content = example_documents_short[2]
+        content = example_documents["short"][1]
         found_doc = find_or_create_document(content)
         self.assertEqual(found_doc.content, content)
         self.assertEqual(str(found_doc), "Document 1")
@@ -36,30 +31,22 @@ class DocumentTests(TestCase):
 class ClusterTests(TestCase):
 
     def test_create_cluster(self):
-        dataset_name = "Test dataset"
-        terms = str(example_terms1)
-        cluster = Cluster(dataset=dataset_name, number=0, terms=terms)
-        cluster.save()
-        self.assertEqual(cluster.dataset, dataset_name)
+        cluster = create_example_cluster()
+        terms = test_dataset["clusters"][0]["terms"]
+        self.assertEqual(cluster.dataset, test_dataset["name"])
         self.assertEqual(cluster.number, 0)
         self.assertEqual(cluster.terms, terms)
 
     def test_assign_reference_document(self):
-        dataset_name = "Test dataset"
-        terms = str(example_terms1)
-        cluster = Cluster(dataset=dataset_name, number=0, terms=terms)
-        cluster.save()
-        reference_document = example_documents_short[0]
+        cluster = create_example_cluster()
+        reference_document = example_documents["short"][0]
         cluster.assign_reference_document(reference_document)
         self.assertEqual(cluster.reference_document.content, reference_document)
 
     def test_add_document(self):
-        dataset_name = "Test dataset"
-        terms = str(example_terms1)
-        cluster = Cluster(dataset=dataset_name, number=0, terms=terms)
-        cluster.save()
-        doc1 = example_documents_short[0]
-        doc2 = example_documents_long[0]
+        cluster = create_example_cluster()
+        doc1 = example_documents["short"][0]
+        doc2 = example_documents["long"][0]
         cluster.add_document(doc1)
         cluster.add_document(doc2)
         cluster_documents = cluster.documents()
