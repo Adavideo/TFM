3627618915efc3a2f94f41559677778bdf108f03
Adavideo <adavideo@gmail.com>
2020-08-03 20:20:48 +0200

    Added navigation through news threads

--- topics/timeline/urls.py
+++ topics/timeline/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.timeline_view, name='timeline'),
+    path('threads', views.threads_list_view, name='threads_list'),
+    path('thread/<thread_id>', views.thread_view, name='thread'),
 ]
--- topics/timeline/views.py
+++ topics/timeline/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from topics_identifier.models import Document
+from .models import Thread
 
 def timeline_view(request):
     template = "timeline.html"
     all_documents = Document.objects.all().order_by("date")
     context = { "documents_list": all_documents}
+    return render(request, template, context)
+
+def threads_list_view(request):
+    template = "threads_list.html"
+    all_threads = Thread.objects.all()
+    context = { "threads_list": all_threads}
+    return render(request, template, context)
+
+def thread_view(request, thread_id):
+    template = "thread.html"
+    thread = Thread.objects.get(id=thread_id)
+    context = { "thread": thread }
     return render(request, template, context)
