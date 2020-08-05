a3f27c3c65b97cda096edb84fed4c56908bf4c46
Adavideo <adavideo@gmail.com>
2020-08-05 11:40:30 +0200

    Fixed test for stop words

--- topics/topics_identifier/tests/test_stop_words.py
+++ topics/topics_identifier/tests/test_stop_words.py
@@ -1,5 +1,9 @@
+from django.test import TestCase
 from topics_identifier.stop_words import get_stop_words
+from .examples import example_stop_words
 
-def test_get_stop_words(self):
-    stop_words = get_stop_words()
-    self.assertEqual(stop_words, example_stop_words)
+class StopWordsTests(TestCase):
+
+    def test_get_stop_words(self):
+        stop_words = get_stop_words()
+        self.assertEqual(stop_words, example_stop_words)
