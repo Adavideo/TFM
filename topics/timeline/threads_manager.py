7cb385cccb98361f3106dbd3afd39d2984b95bc7
Adavideo <adavideo@gmail.com>
2020-08-06 12:02:12 +0200

    Renamed function store_thread

--- topics/timeline/threads_manager.py
+++ topics/timeline/threads_manager.py
@@ -1,7 +1,7 @@
 from .models import Thread
 
 
-def save_thread(thread_number):
+def store_thread(thread_number):
     filename = "missing_news.txt"
     out_file = open(filename, 'a')
     out_file.write(str(thread_number)+", ")
@@ -11,6 +11,6 @@ def check_threads_without_news():
     threads_without_title = Thread.objects.filter(title=None)
     threads_without_news = []
     for thread in threads_without_title:
-        save_thread(thread.number)
+        store_thread(thread.number)
         threads_without_news.append(thread)
     return threads_without_news
