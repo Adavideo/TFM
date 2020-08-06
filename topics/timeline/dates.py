ddf555e75f5a9edffeb81b1ab21b9d50e168e415
Adavideo <adavideo@gmail.com>
2020-08-06 12:00:40 +0200

    Deleted duplicated dates file

--- topics/timeline/dates.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def get_date(date_string):
-    date = datetime.fromisoformat(date_string)
-    date_without_timezone = date.astimezone(tz=None)
-    return date_without_timezone
