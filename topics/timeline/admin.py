0422b0bfbdc5edccb230efabbe6f87db9dc11415
Adavideo <adavideo@gmail.com>
2020-08-07 20:07:17 +0200

    Addign topics and thread_topic to admin site

--- topics/timeline/admin.py
+++ topics/timeline/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Thread, Document
+from .models import Thread, Document, Topic, ThreadTopic
 
 admin.site.register(Thread)
 admin.site.register(Document)
+admin.site.register(Topic)
+admin.site.register(ThreadTopic)
