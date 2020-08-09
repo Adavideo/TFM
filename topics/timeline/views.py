8e1fea16a6c6117387f4455f2f847ee0544c2b26
Adavideo <adavideo@gmail.com>
2020-08-09 18:31:48 +0200

    Moved templates of timeline app to its own directory

--- topics/timeline/views.py
+++ topics/timeline/views.py
@@ -3,25 +3,25 @@ from .models import Thread, Document, Topic
 from .threads_manager import check_threads_without_news
 
 def timeline_view(request):
-    template = "timeline_home.html"
+    template = "timeline/timeline_home.html"
     context = { }
     return render(request, template, context)
 
 def threads_list_view(request):
-    template = "threads_list_page.html"
+    template = "timeline/threads_list_page.html"
     threads_list = Thread.objects.all()
     context = { "threads_list": threads_list }
     return render(request, template, context)
 
 def topic_threads_view(request, topic_id):
-    template = "threads_list_page.html"
+    template = "timeline/threads_list_page.html"
     topic = Topic.objects.get(id=topic_id)
     threads_list = topic.get_threads()
     context = { "threads_list": threads_list }
     return render(request, template, context)
 
 def thread_view(request, thread_id):
-    template = "thread.html"
+    template = "timeline/thread.html"
     thread = Thread.objects.get(id=thread_id)
     news = thread.news()
     comments_list = thread.comments()
@@ -29,13 +29,13 @@ def thread_view(request, thread_id):
     return render(request, template, context)
 
 def check_threads_view(request):
-    template = "check_threads.html"
+    template = "timeline/check_threads.html"
     no_news = check_threads_without_news()
     context = { "threads_without_news": no_news, "number": len(no_news) }
     return render(request, template, context)
 
 def topics_list_view(request):
-    template = "topics_list.html"
+    template = "timeline/topics_list.html"
     topics_list = Topic.objects.all()
     context = { "topics_list": topics_list }
     return render(request, template, context)
