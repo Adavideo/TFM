4bd2d4f4005746f29ae4126a4c36c72024fb5de9
Adavideo <adavideo@gmail.com>
2020-09-01 11:00:59 +0200

    Refactor of models_manager tests

--- topics/topics_identifier/tests/test_models_manager.py
+++ topics/topics_identifier/tests/test_models_manager.py
@@ -7,25 +7,26 @@ from .mock_generators import mock_model, mock_models_manager
 from .validations_models import *
 from .examples import example_documents, test_model_name
 
-manager = mock_models_manager()
 
 class ModelsManagerTests(TestCase):
 
     def test_get_model_filename(self):
-        manager2 = mock_models_manager(name="test")
-        filename = manager2.get_model_filename( level=0)
+        manager = mock_models_manager(name="test")
+        filename = manager.get_model_filename( level=0)
         self.assertEqual(filename, "models/sklearn/test_model_level0.joblib")
 
     def test_get_vectorizer_filename(self):
-        manager2 = mock_models_manager(name="test")
-        filename = manager2.get_vectorizer_filename(level=0)
+        manager = mock_models_manager(name="test")
+        filename = manager.get_vectorizer_filename(level=0)
         self.assertEqual(filename, "models/sklearn/test_vectorizer_level0.joblib")
 
     def test_load_model(self):
+        manager = mock_models_manager()
         model = manager.load_model(level=0)
         self.assertEqual(type(model), type(AffinityPropagation()))
 
     def test_load_vectorizer(self):
+        manager = mock_models_manager()
         vectorizer = manager.load_vectorizer(level=0)
         self.assertEqual(type(vectorizer), type(TfidfVectorizer()))
 
@@ -33,23 +34,27 @@ class ModelsManagerTests(TestCase):
         model_name = "delete_me"
         level = 0
         model = mock_model()
-        manager3 = mock_models_manager(name=model_name)
-        manager3.store_model(model, level)
+        manager = mock_models_manager(name=model_name)
+        manager.store_model(model, level)
         validate_model_stored(self, model_name, level)
 
     def test_store_vectorizer(self):
-        model_name = "delete_me"
+        # Initialize
+        manager1 = mock_models_manager()
         level = 0
-        vectorizer = manager.load_vectorizer(level)
-        manager3 = mock_models_manager(name=model_name)
-        manager3.store_vectorizer(vectorizer, level)
+        vectorizer = manager1.load_vectorizer(level)
+        model_name = "delete_me"
+        manager2 = mock_models_manager(name=model_name)
+        # Execute
+        manager2.store_vectorizer(vectorizer, level)
+        # Validate
         validate_vectorizer_stored(self, model_name, level)
 
     def test_generate_and_store_model_level0(self):
         level = 0
         model_name = "delete_me_2"
-        manager4 = mock_models_manager(name=model_name)
-        model_filename = manager4.generate_and_store_models(example_documents, level)
+        manager = mock_models_manager(name=model_name)
+        model_filename = manager.generate_and_store_models(example_documents, level)
         self.assertEqual(model_filename, "models/sklearn/delete_me_2_model_level0.joblib")
         validate_model_stored(self, model_name, level)
         validate_vectorizer_stored(self, model_name, level)
@@ -58,8 +63,8 @@ class ModelsManagerTests(TestCase):
         level = 1
         model_name = "delete_me_2"
         mock_documents()
-        manager4 = mock_models_manager(name=model_name)
-        model_filename = manager4.generate_and_store_models(example_documents, level)
+        manager = mock_models_manager(name=model_name)
+        model_filename = manager.generate_and_store_models(example_documents, level)
         self.assertEqual(model_filename, "models/sklearn/delete_me_2_model_level1.joblib")
         validate_model_stored(self, model_name, level)
         validate_vectorizer_stored(self, model_name, level)
