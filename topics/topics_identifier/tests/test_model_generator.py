2f4f55b17919f3293fd6d51f1c382dc47aefe53f
Adavideo <adavideo@gmail.com>
2020-09-18 09:23:28 +0200

    Added test for storing reference documents when generating a new model

--- topics/topics_identifier/tests/test_model_generator.py
+++ topics/topics_identifier/tests/test_model_generator.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from .mock_generators import mock_models_manager
-from .validations_models import validate_model_stored, validate_vectorizer_stored
+from .validations_models import *
 from .examples import test_model_name
 from .example_documents import example_documents
 from .examples_documents_selector import example_documents_limit
-
+from .example_trees import example_tree
 
 
 class ModelGeneratorTests(TestCase):
@@ -12,15 +12,19 @@ class ModelGeneratorTests(TestCase):
     def test_generate_model_level0(self):
         level = 0
         model_name = "delete_me_3"
-        manager = mock_models_manager(name=model_name)
-        manager.generate_and_store_models(documents=example_documents, max_level=level)
+        models_manager = mock_models_manager(name=model_name)
+        models_manager.generate_and_store_models(documents=example_documents, max_level=level)
         validate_model_stored(self, model_name, level)
         validate_vectorizer_stored(self, model_name, level)
+        expected = example_tree[level]["reference_documents"]
+        validate_reference_documents_stored(self, model_name, level, expected)
 
     def test_generate_model_level1(self):
         level = 1
         model_name = "delete_me_3"
-        manager = mock_models_manager(name=model_name)
-        manager.generate_and_store_models(documents=example_documents, max_level=level)
+        manager_manager = mock_models_manager(name=model_name)
+        manager_manager.generate_and_store_models(documents=example_documents, max_level=level)
         validate_model_stored(self, model_name, level)
         validate_vectorizer_stored(self, model_name, level)
+        expected = example_tree[level]["reference_documents"]
+        validate_reference_documents_stored(self, model_name, level, expected)
