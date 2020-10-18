b986dd6bbffca2d865395f71a12d8b05e763c60b
Adavideo <adavideo@gmail.com>
2020-10-18 10:38:54 +0200

    Moved mock_thread_with_news to common

--- topics/common/testing/mock_threads.py
+++ topics/common/testing/mock_threads.py
@@ -1,5 +1,5 @@
 from timeline.models import Thread
-from .mock_documents import mock_news, mock_comments
+from .mock_documents import mock_news, mock_comments, mock_document
 
 
 def mock_thread(thread_number, with_documents=False, news_number=None):
@@ -28,3 +28,11 @@ def mock_threads_with_topic(topic):
     thread_list = mock_threads_list()
     topic.assign_threads_list(thread_list)
     return thread_list
+
+def mock_thread_with_news(title, content, thread_number=0):
+    thread = Thread(number=thread_number, title=title)
+    thread.save()
+    news = mock_document(content=title+"\n"+content, is_news=True)
+    news.thread = thread
+    news.save()
+    return thread
--- topics/csv_import/tests/mocks_for_annotated_topics.py
+++ topics/csv_import/tests/mocks_for_annotated_topics.py
@@ -1,14 +1,6 @@
-from timeline.models import Thread, Topic
-from .mocks import mock_document
+from timeline.models import Topic
+from .mocks import mock_thread_with_news
 
-
-def mock_thread_with_news(title, content, thread_number=0):
-    thread = Thread(number=thread_number, title=title)
-    thread.save()
-    news = mock_document(content=title+"\n"+content, is_news=True)
-    news.thread = thread
-    news.save()
-    return thread
 
 def mock_thread_for_annotated_topic(annotation, topic_name, thread_number=0):
     title = annotation[0]
