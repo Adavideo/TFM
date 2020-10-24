3b21597ec2944b1f4bc2ba20cc76900549440742
Adavideo <adavideo@gmail.com>
2020-10-24 12:12:41 +0200

    Renamed proyect to Debate Classifier

--- debate_classifier/manage.py
+++ debate_classifier/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'topics.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debate_classifier.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
